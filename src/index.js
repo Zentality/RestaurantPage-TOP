b7912b86eec05741b501d838cdad67de2e07f398
Zentality <jmcp@live.com.au>
2022-05-16 16:57:22 +0800

    Add logic to change tabs

--- src/index.js
+++ src/index.js
@@ -8,3 +8,25 @@ page.appendChild(mainPage);
 let contentBlock = document.querySelector(".tab");
 
 contentBlock.appendChild(homePage);
+
+let navButtons = document.querySelectorAll(".nav span");
+
+navButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    navButtons.forEach((buttonDeactivate) => buttonDeactivate.classList.remove("active"));
+    button.classList.add("active");
+
+    switch (event.target.textContent) {
+      case "Home":
+        contentBlock.textContent = "";
+        contentBlock.appendChild(homePage);
+        break;
+      case "Menu":
+        contentBlock.textContent = "";
+        break;
+      case "Contact":
+        contentBlock.textContent = "";
+        break;
+    }
+  })
+})
